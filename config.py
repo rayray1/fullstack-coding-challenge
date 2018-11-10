c6e05407a47f198ccf3c89014054f4f711a52a90
Raymond Rupiah <rupiray@yahoo.com>
2018-11-10 18:37:09 +0000

    added test-config

--- config.py
+++ config.py
@@ -2,15 +2,28 @@ import os
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
+    DEBUG = True
     SECRET_KEY = 'asdfgfhhfhgfgjjfdfddvgbbfgnf'  # should be moved to environment variables in production
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://slwukubt:WcTJ9PLNSTBu9_s0wTFCCbwE3BFdd6J8@stampy.db.elephantsql.com:5432/slwukubt'
+    SQLALCHEMY_DATABASE_URI = \
+        'postgres://slwukubt:WcTJ9PLNSTBu9_s0wTFCCbwE3BFdd6J8@stampy.db.elephantsql.com:5432/slwukubt'
+    DEBUG = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://slwukubt:WcTJ9PLNSTBu9_s0wTFCCbwE3BFdd6J8@stampy.db.elephantsql.com:5432/slwukubt'
+    SQLALCHEMY_DATABASE_URI = \
+        'postgres://slwukubt:WcTJ9PLNSTBu9_s0wTFCCbwE3BFdd6J8@stampy.db.elephantsql.com:5432/slwukubt'
+
+
+class TestingConfig(object):
+    TESTING = True
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
